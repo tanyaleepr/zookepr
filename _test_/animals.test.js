f08ae43a3110c263eb71421b5be61602c4fc44a8
tanyaleepr <tanyaleepr@gmail.com>
2022-02-24 12:09:21 -0600

    deb

--- _test_/animals.test.js
+++ _test_/animals.test.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../lib/animals.js');
-const { animals } = require('../data/animals');
+const { animalsArray } = require('../data/animals');
 
 test('creates an animal object', () => {
-  const animal = createNewAnimal({ name: 'Darlene', id: 'jhgdja3ng2' }, animals);
+  const animal = createNewAnimal({ name: 'Darlene', id: 'jhgdja3ng2' }, animalsArray);
 
   expect(animal.name).toBe('Darlene');
   expect(animal.id).toBe('jhgdja3ng2');
